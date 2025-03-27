c678f3bf199699690ece13ffd3951a54162b66b7
deren <de2356@columbia.edu>
2025-03-27 10:58:23 -0400

    paired program DE

--- src/module.py
+++ src/module.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python
+
+"""Initial functions to read and process input data for M6Anet.
+
+"""
+
+def read_ont_bam_file(bam_file):
+	"""Process an input ONT bam file to do..."""
+
+
+def read_ont_fasta_file(fasta_file):
+	"""Process an input fasta file to do..."""
+
+
+
+
+
+
+
+if __name__ == "__main__":
+	pass
